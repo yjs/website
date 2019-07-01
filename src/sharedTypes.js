67d55430427f29ba5a0ca86ee58187384b136a8d
Kevin Jahns <kevin.jahns@protonmail.com>
2019-07-01 23:25:12 +0200

    ws url to yjs-demos.now.sh

--- src/sharedTypes.js
+++ src/sharedTypes.js
@@ -3,8 +3,8 @@ import * as Y from 'yjs'
 import { WebsocketProvider } from 'y-websocket'
 
 export const doc = new Y.Doc()
-console.log('ws://localhost:1234')
-export const provider = new WebsocketProvider('ws://localhost:1234', 'yjs.dev', doc)
+const url = location.hostname === 'localhost' || location.hostname === '127.0.0.1' ? 'ws://localhost:1234' : 'wss://yjs-demos.now.sh'
+export const provider = new WebsocketProvider(url, 'yjs-website', doc)
 export const awareness = provider.awareness
 export const prosemirrorEditorContent = /** @type {Y.XmlFragment} */ (doc.get('prosemirror', Y.XmlFragment))
 
