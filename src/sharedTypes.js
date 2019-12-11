9bb69f814903c8239673ef6336594d6c51b2d743
Kevin Jahns <kevin.jahns@protonmail.com>
2019-12-11 15:43:45 +0100

    use y-webrtc

--- src/sharedTypes.js
+++ src/sharedTypes.js
@@ -1,10 +1,9 @@
 
 import * as Y from 'yjs'
-import { WebsocketProvider } from 'y-websocket'
+import { WebrtcProvider } from 'y-webrtc'
 
 export const doc = new Y.Doc()
-const url = location.hostname === 'localhost' || location.hostname === '127.0.0.1' ? 'ws://localhost:1234' : 'wss://yjs-demos.now.sh'
-export const provider = new WebsocketProvider(url, 'yjs-website', doc)
+export const provider = new WebrtcProvider('yjs-website', doc)
 export const awareness = provider.awareness
 export const prosemirrorEditorContent = /** @type {Y.XmlFragment} */ (doc.get('prosemirror', Y.XmlFragment))
 /**
